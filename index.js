778e0fceccf3343c52319faddb07743b454a8d2f
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-08-16 05:01:17 -0700

    complete untill deployment

--- index.js
+++ index.js
@@ -38,9 +38,6 @@ function getCurrentTime() {
 
 setInterval(getCurrentTime, 1000)
 
-
-
-
 navigator.geolocation.getCurrentPosition(position => {
     fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`)
         .then(res => {
@@ -50,12 +47,11 @@ navigator.geolocation.getCurrentPosition(position => {
             return res.json()
         })
         .then(data => {
-            console.log(data)
             const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
             document.getElementById("weather").innerHTML = `
                 <img src=${iconUrl} />
-                <p>${Math.round(data.main.temp)}º</p>
-                <p>${data.name}</p>
+                <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+                <p class="weather-city">${data.name}</p>
             `
         })
         .catch(err => console.error(err))
