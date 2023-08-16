e9e78835448bacd5cecb3ba3aad7964cd902588d
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-08-16 04:29:04 -0700

    get users current weather data

--- index.js
+++ index.js
@@ -37,3 +37,20 @@ function getCurrentTime() {
 }
 
 setInterval(getCurrentTime, 1000)
+
+
+
+
+navigator.geolocation.getCurrentPosition(position => {
+    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`)
+        .then(res => {
+            if (!res.ok) {
+                throw Error("Weather data not available")
+            }
+            return res.json()
+        })
+        .then(data => {
+            console.log(data)
+        })
+        .catch(err => console.error(err))
+});
